b60a33c90d39b61c855c06faf6a7bc88579e3ffd
Valdiran de Andrade <valdiran.silva@renovsmart.com.br>
2025-08-11 15:11:18 -0300

    IMAGE FORMAT UPDATE: Trocar bg-como-funciona.webp para bg-como-funciona.png

--- litespeed-cache-optimizer.js
+++ litespeed-cache-optimizer.js
@@ -163,7 +163,7 @@ class LiteSpeedCacheOptimizer {
                     left: 0; 
                     width: 100vw; 
                     height: 100%; 
-                    background-image: url('assets/images/mobile/bg-como-funciona.webp'); 
+                    background-image: url('assets/images/mobile/bg-como-funciona.png'); 
                     background-size: cover; 
                     background-position: center; 
                     background-repeat: no-repeat; 
@@ -313,7 +313,7 @@ class LiteSpeedCacheOptimizer {
                     'assets/images/Renov-Logo.webp',
                     'assets/fonts/Montserrat-Regular.woff2',
                     'assets/fonts/Montserrat-Medium.woff2',
-                    'assets/images/mobile/bg-como-funciona.webp'
+                    'assets/images/mobile/bg-como-funciona.png'
                 ];
                 
                 criticalResources.forEach(resource => {
